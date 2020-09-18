9d086a996104b9850b984ffe6aa14c3990428869
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-09-18 21:25:35 +0200

    Implemented menu and added tests

---
+++ frontend/src/components/Menu.test.tsx
@@ -0,0 +1,198 @@
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import { CreateOrder, Menu } from "../api.types";
+import * as store from "../store";
+import { MenuComponent } from "./Menu";
+
+const TEST_MENU: Menu = [
+  {
+    id: 1,
+    name: "Menu Item 1",
+    price: 4000,
+    type: "main",
+  },
+  {
+    id: 2,
+    name: "Menu Item 2",
+    price: 2000,
+    type: "drink",
+  },
+  {
+    id: 3,
+    name: "Menu Item 3",
+    price: 1000,
+    type: "dessert",
+  },
+];
+
+const TEST_ORDER: CreateOrder = {
+  order: [],
+  price: 0,
+  isDeal: false,
+};
+
+let spyDispatch: jest.SpyInstance;
+
+beforeEach(() => {
+  jest.spyOn(store, "useAppState").mockReturnValue({ menu: TEST_MENU, order: TEST_ORDER } as any);
+  spyDispatch = jest.fn();
+  jest.spyOn(store, "useDispatch").mockReturnValue(spyDispatch as any);
+});
+
+test("check title exists", () => {
+  const { getByText } = render(<MenuComponent />);
+  const titleElement = getByText("Menu");
+  expect(titleElement).toBeInTheDocument();
+});
+
+test("check menu list exists", () => {
+  const { getByTestId } = render(<MenuComponent />);
+  const listElement = getByTestId("menu-list");
+  expect(listElement).toBeInTheDocument();
+});
+
+test("input dispatches correct action", () => {
+  const { getByTestId } = render(<MenuComponent />);
+  const inputElement = getByTestId("input-2");
+  expect(inputElement).toBeInTheDocument();
+  fireEvent.change(inputElement.firstElementChild as HTMLInputElement, {
+    target: { value: "3" },
+  });
+  expect(spyDispatch).toHaveBeenCalledWith({
+    type: "addOrderItem",
+    payload: { menuId: 2, quantity: 3 },
+  });
+});
+
+test("list is rendered properly", () => {
+  const { getByTestId } = render(<MenuComponent />);
+  const listElement = getByTestId("menu-list");
+  expect(listElement).toBeInTheDocument();
+  expect(listElement).toMatchInlineSnapshot(`
+    <div
+      class="MuiList-root MuiList-padding"
+      data-testid="menu-list"
+    >
+      <li
+        class="MuiListItem-root MuiListItem-gutters"
+      >
+        <div
+          class="MuiListItemText-root"
+        >
+          <span
+            class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock"
+          >
+            MAIN
+          </span>
+        </div>
+      </li>
+      <div
+        class="MuiList-root MuiList-dense"
+      >
+        <li
+          class="MuiListItem-root makeStyles-nested-4 MuiListItem-dense MuiListItem-gutters"
+        >
+          <div
+            class="MuiListItemText-root MuiListItemText-dense"
+          >
+            <span
+              class="MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock"
+            >
+              Menu Item 1
+            </span>
+          </div>
+          <div
+            class="MuiInputBase-root MuiInput-root MuiInput-underline"
+            data-testid="input-1"
+          >
+            <input
+              class="MuiInputBase-input MuiInput-input"
+              type="number"
+              value="0"
+            />
+          </div>
+        </li>
+      </div>
+      <li
+        class="MuiListItem-root MuiListItem-gutters"
+      >
+        <div
+          class="MuiListItemText-root"
+        >
+          <span
+            class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock"
+          >
+            DRINK
+          </span>
+        </div>
+      </li>
+      <div
+        class="MuiList-root MuiList-dense"
+      >
+        <li
+          class="MuiListItem-root makeStyles-nested-4 MuiListItem-dense MuiListItem-gutters"
+        >
+          <div
+            class="MuiListItemText-root MuiListItemText-dense"
+          >
+            <span
+              class="MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock"
+            >
+              Menu Item 2
+            </span>
+          </div>
+          <div
+            class="MuiInputBase-root MuiInput-root MuiInput-underline"
+            data-testid="input-2"
+          >
+            <input
+              class="MuiInputBase-input MuiInput-input"
+              type="number"
+              value="0"
+            />
+          </div>
+        </li>
+      </div>
+      <li
+        class="MuiListItem-root MuiListItem-gutters"
+      >
+        <div
+          class="MuiListItemText-root"
+        >
+          <span
+            class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock"
+          >
+            DESSERT
+          </span>
+        </div>
+      </li>
+      <div
+        class="MuiList-root MuiList-dense"
+      >
+        <li
+          class="MuiListItem-root makeStyles-nested-4 MuiListItem-dense MuiListItem-gutters"
+        >
+          <div
+            class="MuiListItemText-root MuiListItemText-dense"
+          >
+            <span
+              class="MuiTypography-root MuiListItemText-primary MuiTypography-body2 MuiTypography-displayBlock"
+            >
+              Menu Item 3
+            </span>
+          </div>
+          <div
+            class="MuiInputBase-root MuiInput-root MuiInput-underline"
+            data-testid="input-3"
+          >
+            <input
+              class="MuiInputBase-input MuiInput-input"
+              type="number"
+              value="0"
+            />
+          </div>
+        </li>
+      </div>
+    </div>
+  `);
+});
--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,22 +1,76 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+import Input from "@material-ui/core/Input";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
-import { Link } from "react-router-dom";
-import { useAppState } from "../store";
+import { CreateOrder, Menu, MenuItem } from "../api.types";
+import { useAppState, useDispatch } from "../store";
+import { DealsComponent } from "./Deals";
 
-export function Menu() {
-  const { menu } = useAppState();
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+  })
+);
+
+export function MenuComponent() {
+  const classes = useStyles();
+  const { menu, order } = useAppState();
+  const dispatch = useDispatch();
+
+  const nestMenu = (menu: Menu): { type: string; items: MenuItem[] }[] =>
+    menu.reduce((acc, curr) => {
+      const typeItems = acc.find((elem) => elem.type === curr.type);
+      if (typeItems) {
+        typeItems.items.push(curr);
+      } else {
+        acc.push({ type: curr.type, items: [curr] });
+      }
+      return acc;
+    }, [] as { type: string; items: MenuItem[] }[]);
+
+  const getOrderedQuantity = (id: number, order: CreateOrder): number => {
+    const ordered = order.order.find((o) => o.menuId === id);
+    return ordered ? ordered.quantity : 0;
+  };
+
   return (
     <div>
-      <span>Menu</span>
-      {menu && menu.map((item) => (
-        <div key={item.id}>
-          <span>{item.id}</span> <span>{item.name}</span>
-        </div>
-      ))}
-      <div>
-        <Link to="/order">
-          <button>Click</button>
-        </Link>
-      </div>
+      <Typography variant="h4" component="h1">
+        Menu
+      </Typography>
+
+      {menu && (
+        <List component="div" data-testid="menu-list">
+          {nestMenu(menu).map((type) => (
+            <React.Fragment key={type.type}>
+              <ListItem>
+                <ListItemText primary={type.type.toUpperCase()} />
+              </ListItem>
+              <List component="div" disablePadding dense={true}>
+                {type.items.map((item) => (
+                  <ListItem key={item.id} className={classes.nested}>
+                    <ListItemText primary={item.name} />
+                    <Input
+                      defaultValue={getOrderedQuantity(item.id, order)}
+                      type="number"
+                      onChange={(evt) =>
+                        dispatch({ type: "addOrderItem", payload: { menuId: item.id, quantity: +evt.target.value } })
+                      }
+                      data-testid={`input-${item.id}`}
+                    />
+                  </ListItem>
+                ))}
+              </List>
+            </React.Fragment>
+          ))}
+        </List>
+      )}
+      <DealsComponent />
     </div>
   );
 }
