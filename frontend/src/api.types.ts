514da244f66147b92fec757e9d1d5461b0997d72
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-09-18 08:04:52 +0200

    Created API types

---
+++ frontend/src/api.types.ts
@@ -0,0 +1,9 @@
+export type MenuItem = { id: number; name: string; price: number; type: string };
+export type Menu = MenuItem[];
+
+export type Condition = { type: string; quantity: number };
+export type Deal = { id: number; name: string; price: number; conditions: Condition[] };
+
+export type OrderItem = { menuId: number; quantity: number };
+export type Order = { id: number; order: OrderItem[]; price: number; waitTime: number };
+export type CreateOrder = { order: OrderItem[]; price: number; isDeal: boolean; deal?: Deal };
