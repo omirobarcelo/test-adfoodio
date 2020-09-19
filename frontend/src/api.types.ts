034c12f64b9100c3465cd489819a46b7a4f3e8ac
Oriol Miro-Barcelo <oriol.barcelo@pantabio.com>
2020-09-20 00:12:45 +0200

    Updated api types to new pick up time

--- frontend/src/api.types.ts
+++ frontend/src/api.types.ts
@@ -5,5 +5,5 @@ export type Condition = { type: string; quantity: number };
 export type Deal = { id: number; name: string; price: number; conditions: Condition[] };
 
 export type OrderItem = { menuId: number; quantity: number };
-export type Order = { id: number; order: OrderItem[]; price: number; waitTime: number };
+export type Order = { id: number; order: OrderItem[]; price: number; pickUpTime: Date };
 export type CreateOrder = { order: OrderItem[]; price: number; isDeal: boolean; deal?: Deal };
